6f847f03529acdb42482dc1cfc885d28a2d9dbd7
jean <jean.carla.guarra@gmail.com>
2026-02-24 17:32:17 +0800

    feat: replace light icon with dev.svg and add dark mode class to HTML

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,16 +15,8 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/dev.svg',
         media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
       },
     ],
     apple: '/apple-icon.png',
@@ -43,7 +35,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className='dark'>
       <body className="font-sans antialiased" suppressHydrationWarning>
         {children}
         <Analytics />
